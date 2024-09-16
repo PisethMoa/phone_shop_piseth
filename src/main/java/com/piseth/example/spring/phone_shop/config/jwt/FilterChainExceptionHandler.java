c8f21ec8147d8dd4bc9585950c9b4ed3ad8bb2d1
PisethMao <126128991+PisethMoa@users.noreply.github.com>
2024-09-16 20:10:02 +0700

    add Filter Exception, Unit Test on Brand Update

---
+++ src/main/java/com/piseth/example/spring/phone_shop/config/jwt/FilterChainExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.piseth.example.spring.phone_shop.config.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Slf4j
+@Component
+public class FilterChainExceptionHandler extends OncePerRequestFilter {
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver handlerExceptionResolver;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        try {
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            logger.error("Spring Security Filter Chain Exception: ", e);
+            handlerExceptionResolver.resolveException(request, response, null, e);
+        }
+    }
+}
